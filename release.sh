f916e1030df50b10438f3d382e4aa8d24400b924
spiltcoffee <spiltcoffee@users.noreply.github.com>
2025-04-16 22:27:27 +1000

    Generate release notes as part of release process

--- release.sh
+++ release.sh
@@ -11,9 +11,14 @@ version="$(node -p "require('./package.json').version")"
 npm run format-changelog
 npm run bump-changelog "$version"
 npm run format-changelog
+
 git add CHANGELOG.md package.json package-lock.json
 git commit -m "v$version"
 git tag "v$version"
 git push origin --atomic main "v$version"
+
+changelog="$(npm run get-latest-changelog)"
+
+gh release "v$version" --title "v$version" --notes "$changelog"
 
 ./build-and-push-docker.sh $version
