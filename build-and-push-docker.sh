4989fb254f1c3ae1425011c701f1d0dba967bb12
spiltcoffee <spiltcoffee@users.noreply.github.com>
2025-04-16 22:15:43 +1000

    Fix release scripts

---
+++ build-and-push-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+
+version="$1"
+
+function build-and-push-docker() (
+  local container="$1"
+  local version="$2"
+  cd "$container"
+  docker build -t "ghcr.io/yellow-corps/ibis-$container:$version" -f Containerfile .
+  docker push "ghcr.io/yellow-corps/ibis-$container:$version"
+)
+
+build-and-push-docker discord "v$version"
+build-and-push-docker discord latest
+
+build-and-push-docker exporter "v$version"
+build-and-push-docker exporter latest
+
+build-and-push-docker shopify "v$version"
+build-and-push-docker shopify latest
--- release.sh
+++ release.sh
@@ -16,19 +16,4 @@ git commit -m "v$version"
 git tag "v$version"
 git push origin --atomic main "v$version"
 
-function build-and-push-docker() (
-  local container="$1"
-  local $version="$2"
-  cd "$container"
-  docker build -t "ghcr.io/yellow-corps/ibis-$container:$version" -f Containerfile .
-  docker push "ghcr.io/yellow-corps/ibis-$container:$version"
-)
-
-build-and-push-docker discord "v$version"
-build-and-push-docker discord latest
-
-build-and-push-docker exporter "v$version"
-build-and-push-docker exporter latest
-
-build-and-push-docker shopify "v$version"
-build-and-push-docker shopify latest
+./build-and-push-docker.sh $version
