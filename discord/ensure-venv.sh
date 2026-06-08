cce2813a2f32024f8d340ad5ae68eb38a9e9d6a3
spiltcoffee <spiltcoffee@users.noreply.github.com>
2026-06-08 16:28:33 +1000

    Introduce dev python dependencies

--- discord/ensure-venv.sh
+++ discord/ensure-venv.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-set -o errexit
-
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-ACTIVATE_SCRIPT="$BASE_DIR/.venv/Scripts/activate"
+VENV_DIR="$BASE_DIR/.venv"
+ACTIVATE_SCRIPT="$VENV_DIR/Scripts/activate"
 
-if [ ! -f "$ACTIVATE_SCRIPT" ]; then
-  python -m venv .venv --prompt ibis/.venv
-  # shellcheck disable=SC1090
-  source "$ACTIVATE_SCRIPT"
-  pip install -r requirements.txt --ignore-requires-python
-elif [ -z "$VIRTUAL_ENV" ]; then
-  # shellcheck disable=SC1090
-  source "$ACTIVATE_SCRIPT"
-fi
+function __ensure_python() {
+  unset __ensure_python
+  if [ ! -f "$ACTIVATE_SCRIPT" ] || [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+    rm -rf "$VENV_DIR"
+    python -m venv "$VENV_DIR" --prompt ibis/.venv || return 1
+    # shellcheck disable=SC1090
+    source "$ACTIVATE_SCRIPT"
+    pip install -r "$BASE_DIR/requirements.dev.txt" --ignore-requires-python || return 1
+  elif [ -z "$VIRTUAL_ENV" ]; then
+    # shellcheck disable=SC1090
+    source "$ACTIVATE_SCRIPT"
+  fi
+}
+__ensure_python "$@"
