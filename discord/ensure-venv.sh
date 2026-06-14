ad214c3de1fc9272616a5bbb8c763d46378fd986
spiltcoffee <3926298+spiltcoffee@users.noreply.github.com>
2026-06-14 22:44:18 +1000

    Make ensure-venv.sh work on Unix systems

--- discord/ensure-venv.sh
+++ discord/ensure-venv.sh
@@ -2,17 +2,22 @@
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 VENV_DIR="$BASE_DIR/.venv"
-ACTIVATE_SCRIPT="$VENV_DIR/Scripts/activate"
+
+if [[ "$(uname -s)" == "Windows" ]]; then
+  ACTIVATE_SCRIPT="$VENV_DIR/Scripts/activate"
+else
+  ACTIVATE_SCRIPT="$VENV_DIR/bin/activate"
+fi
 
 function __ensure_python() {
   unset __ensure_python
-  if [ ! -f "$ACTIVATE_SCRIPT" ] || [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+  if [[ ! -f "$ACTIVATE_SCRIPT" ]] || [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
     rm -rf "$VENV_DIR"
     python -m venv "$VENV_DIR" --prompt ibis/.venv || return 1
     # shellcheck disable=SC1090
     source "$ACTIVATE_SCRIPT"
     pip install -r "$BASE_DIR/requirements.dev.txt" --ignore-requires-python || return 1
-  elif [ -z "$VIRTUAL_ENV" ]; then
+  elif [[ -z "$VIRTUAL_ENV" ]]; then
     # shellcheck disable=SC1090
     source "$ACTIVATE_SCRIPT"
   fi
