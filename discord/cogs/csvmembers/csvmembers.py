88d9af318e3346cc853f89874a86387d6f758412
spiltcoffee <3926298+spiltcoffee@users.noreply.github.com>
2026-06-15 10:09:01 +1000

    Cover CSV Members cog fully with tests

--- discord/cogs/csvmembers/csvmembers.py
+++ discord/cogs/csvmembers/csvmembers.py
@@ -5,27 +5,8 @@ import discord
 import ibis
 
 
-class CsvMembersCog(commands.Cog):
-    def __init__(self):
-        self.config = Config.get_conf(
-            self, identifier=778440330267960653, force_registration=True
-        )
-
-    @commands.command()
-    @commands.admin()
-    async def csvmembers(self, ctx: commands.Context):
-        "Generate a CSV of all members in the server"
-
-        async with ctx.typing():
-            try:
-                csv_file = self.build_csv(ctx.guild)
-                await ibis.reply.success(ctx, file=csv_file)
-            except Exception as ex:
-                await ibis.reply.fail(
-                    ctx, "Building member CSV failed, please see log."
-                )
-                raise ex
-
+# pylint: disable-next=too-few-public-methods
+class CsvMembersBuilder:
     def build_csv(self, guild: discord.Guild) -> discord.File:
         fp = StringIO()
         writer = csv.writer(fp)
@@ -52,3 +33,28 @@ class CsvMembersCog(commands.Cog):
 
         fp.seek(0)
         return discord.File(fp=fp, filename=ibis.file.unique("members.csv"))
+
+
+class CsvMembersCog(commands.Cog):
+    def __init__(self, builder_cls=CsvMembersBuilder):
+        self.config = Config.get_conf(
+            self, identifier=778440330267960653, force_registration=True
+        )
+        self.builder = builder_cls()
+
+    @commands.command()
+    @commands.admin()
+    async def csvmembers(self, ctx: commands.Context):
+        "Generate a CSV of all members in the server"
+
+        print(ctx)
+
+        async with ctx.typing():
+            try:
+                csv_file = self.builder.build_csv(ctx.guild)
+                await ibis.reply.success(ctx, file=csv_file)
+            except Exception as ex:
+                await ibis.reply.fail(
+                    ctx, "Building member CSV failed, please see log."
+                )
+                raise ex
