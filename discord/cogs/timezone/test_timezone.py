a79604508137c5d1c1af345e6588c54b39165f51
spiltcoffee <spiltcoffee@users.noreply.github.com>
2026-08-02 14:02:54 +1000

    Improve tests to use pytest parametrize

--- discord/cogs/timezone/test_timezone.py
+++ discord/cogs/timezone/test_timezone.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from unittest import mock
 from zoneinfo import ZoneInfo
 from redbot.core import Config
@@ -8,42 +9,60 @@ from .timezone import TimeZoneCog
 
 
 @pytest.mark.asyncio
-async def test_get_timezone():
+@pytest.mark.parametrize(
+    argnames=("return_value", "expected_value"),
+    argvalues=[
+        ("America/New_York", ZoneInfo("America/New_York")),
+        ("Not a timezone", None),
+    ],
+)
+async def test_get_timezone(return_value: str, expected_value: Optional[ZoneInfo]):
     config = mock.Mock(Config)
     cog = TimeZoneCog(config)
 
     config.timezone = mock.AsyncMock(Value)
-    config.timezone.return_value = "America/New_York"
-    assert await cog.get_timezone() == ZoneInfo("America/New_York")
+    config.timezone.return_value = return_value
+    assert await cog.get_timezone() == expected_value
     config.timezone.assert_awaited_once()
 
-    config.timezone.reset_mock()
-    config.timezone.return_value = "Not a timezone"
-    assert await cog.get_timezone() is None
-    config.timezone.assert_awaited_once()
 
-    config.timezone.reset_mock()
+@pytest.mark.asyncio
+async def test_get_timezone_error():
+    config = mock.Mock(Config)
+    cog = TimeZoneCog(config)
+
+    config.timezone = mock.AsyncMock(Value)
     config.timezone.side_effect = Exception("test exception")
     assert await cog.get_timezone() is None
     config.timezone.assert_awaited_once()
 
 
 @pytest.mark.asyncio
-async def test_set_timezone():
+@pytest.mark.parametrize(
+    argnames=("set_value", "expected_argument"),
+    argvalues=[(None, None), (ZoneInfo("America/New_York"), "America/New_York")],
+)
+async def test_set_timezone(
+    set_value: Optional[ZoneInfo], expected_argument: Optional[str]
+):
     config = mock.Mock(Config)
     cog = TimeZoneCog(config)
 
     config.timezone = mock.AsyncMock(Value)
-    await cog.set_timezone(None)
-    config.timezone.set.assert_awaited_once_with(None)
-
-    config.timezone.reset_mock()
-    await cog.set_timezone(ZoneInfo("America/New_York"))
-    config.timezone.set.assert_awaited_once_with("America/New_York")
+    await cog.set_timezone(set_value)
+    config.timezone.set.assert_awaited_once_with(expected_argument)
 
 
 @pytest.mark.asyncio
-async def test_command_get():
+@pytest.mark.parametrize(
+    argnames=("return_value", "expected_message"),
+    argvalues=[
+        ("America/New_York", "Timezone: America/New_York"),
+        ("Not a timezone", "Timezone: None"),
+        (None, "Timezone: None"),
+    ],
+)
+async def test_command_get(return_value: Optional[str], expected_message: str):
     config = mock.Mock(Config)
     cog = TimeZoneCog(config)
 
@@ -51,22 +70,10 @@ async def test_command_get():
     ctx = mock.AsyncMock(Context)
 
     with mock.patch("ibis.reply.success") as mock_success:
-        config.timezone.return_value = "America/New_York"
+        config.timezone.return_value = return_value
         # pylint: disable-next=too-many-function-args
         await cog.timezone_get(cog, ctx)
-        mock_success.assert_called_once_with(ctx, "Timezone: America/New_York")
-
-    with mock.patch("ibis.reply.success") as mock_success:
-        config.timezone.return_value = "Not a timezone"
-        # pylint: disable-next=too-many-function-args
-        await cog.timezone_get(cog, ctx)
-        mock_success.assert_called_once_with(ctx, "Timezone: None")
-
-    with mock.patch("ibis.reply.success") as mock_success:
-        config.timezone.return_value = None
-        # pylint: disable-next=too-many-function-args
-        await cog.timezone_get(cog, ctx)
-        mock_success.assert_called_once_with(ctx, "Timezone: None")
+        mock_success.assert_called_once_with(ctx, expected_message)
 
 
 @pytest.mark.asyncio
